47343f82968d633e37b96cdac57cfeaa7ce0c6f7
Brayden <brayburl224@gmail.com>
2022-10-23 10:50:11 -0400

    created code for other classes and required them in index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const inquirer = require('inquirer');
 
 const fs = require('fs');
 
+const Employee = require("./lib/Employee");
+
+const Manager = require("./lib/Manager");
+
+const Engineer = require("./lib/Engineer");
+
+const Intern = require("./lib/Intern");
+
 const generateHTML = ({ name, location, github, linkedin }) =>
   `<!DOCTYPE html>
 <html lang="en">
